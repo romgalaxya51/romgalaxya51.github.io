34af9b342a50573c9bb369d450de08ea5b359d8b
romgalaxya51 <retasaja07@gmail.com>
2025-07-24 20:41:06 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './css/styles.css';
 
+// 🔁 Restore path setelah reload dari 404.html
+const redirectPath = sessionStorage.redirect;
+if (redirectPath) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirectPath);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
